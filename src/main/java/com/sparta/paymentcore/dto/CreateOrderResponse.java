f259eb7c05c585d61cfa24deb9d65ae1113e6c02
dev-robbie <125817809+dev-robbie331@users.noreply.github.com>
2026-04-25 11:28:48 +0900

    Update CreateOrderResponse.java

--- src/main/java/com/sparta/paymentcore/dto/CreateOrderResponse.java
+++ src/main/java/com/sparta/paymentcore/dto/CreateOrderResponse.java
@@ -2,6 +2,7 @@ package com.sparta.paymentcore.dto;
 
 public record CreateOrderResponse(
         Long orderId,
+        Long paymentId,
         String portonePaymentId,
         int totalAmount,
         String orderName,
